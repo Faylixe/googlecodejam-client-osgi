8550ead7a40316dafe0523b8874f0872cf9e1827
fv <fv@192.168.1.30>
2015-12-10 11:20:14 +0100

    Removes sources, in order to make this artifact, a simple OSGi export of the non OSGi googlecodejam-client dependencies.

--- src/main/java/fr/faylixe/googlecodejam/client/executor/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * <p>This package provides tools and facilities for manipulating
- * HTTP request in a Code Jam context.</p>
- * 
- * @author fv
- */
-package fr.faylixe.googlecodejam.client.executor;
